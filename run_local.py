91cd2e4d274fd1b638d733741200f793106439bc
Gordon Watts <gwatts@uw.edu>
2018-12-07 00:48:45 -0800

    Runs locally ok now

--- run_local.py
+++ run_local.py
@@ -1,4 +1,21 @@
 # Run a local copy of the parsl work flow.
 import workflow
+import parsl
 
-print ("hi")
+# Be explicit about loading a threading executor.
+# (copied from the tutorial)
+from parsl.config import Config
+from parsl.executors.threads import ThreadPoolExecutor
+
+local_config = Config(
+    executors=[
+        ThreadPoolExecutor(
+            max_threads=8,
+            label='local_threads'
+        )
+    ]
+)
+parsl.load(local_config)
+
+r = workflow.run_cat_test().result()
+print ("Result from the test is {}".format(r))
--- workflow.py
+++ workflow.py
@@ -1,1 +1,30 @@
 import parsl
+from parsl.app.app import python_app, bash_app
+
+# Start with the cat from the tutorial
+# Will concatenate all of its inputs into a single output file.
+@bash_app
+def cat(inputs=[], outputs=[]):
+    return 'cat %s > %s' %(" ".join(inputs), outputs[0])
+
+# Write out a small text files
+def write_test_text_file (fName, text):
+    with open(fName, 'w') as f:
+        f.write(text)
+        f.close()
+
+@python_app
+def run_cat_test():
+    # Write the files out
+    write_test_text_file ('/tmp/hello1.txt', 'hi1')
+    write_test_text_file ('/tmp/hello2.txt', 'hi2')
+    write_test_text_file ('/tmp/hello3.txt', 'hi3')
+
+    concat = cat(inputs=['/tmp/hello1.txt','/tmp/hello2.txt','/tmp/hello3.txt'],
+             outputs=['all_hellos.txt'])
+
+    # Open the concatenated file
+    with open(concat.outputs[0].result(), 'r') as f:
+        print(f.read())
+
+    return 0
